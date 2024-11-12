0f30192269168a609024aad0faef0aaf9dd63a41
michal maderak <michal.maderak@student.pk.edu.pl>
2024-11-12 12:29:13 +0100

    first

---
+++ PFE_lab03_maderak_michal/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    <>
+     
+     <h1>Lab03</h1>
+
+    </>
+  )
+}
+
+export default App
